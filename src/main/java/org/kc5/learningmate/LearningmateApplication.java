c2d308b7c444094a550624ef587a159a46c8d54f
hs917ouo <hs917ouo@naver.com>
2025-07-31 17:37:24 +0900

    first commit

---
+++ src/main/java/org/kc5/learningmate/LearningmateApplication.java
@@ -0,0 +1,13 @@
+package org.kc5.learningmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningmateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kc5/learningmate/LearningmateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kc5.learningmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
