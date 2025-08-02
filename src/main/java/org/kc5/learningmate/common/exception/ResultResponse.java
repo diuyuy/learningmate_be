417d3115c6bdc6a70a115a85d4d723b1d4b840f3
hyeji <hyeji6742@naver.com>
2025-08-02 19:18:44 +0900

    setting: globalExceptionHandler 추가

---
+++ src/main/java/org/kc5/learningmate/common/exception/ResultResponse.java
@@ -0,0 +1,24 @@
+package org.kc5.learningmate.common.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class ResultResponse<T> implements Serializable {
+    private int status;
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+
+    public ResultResponse(HttpStatus status, String message) {
+        this.status = status.value();
+        this.message = message;
+    }
+}
