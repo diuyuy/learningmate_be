cb1ab502dd12bc32cd30a57218f91f52d0001535
diuyuy <yshyun0105@gmail.com>
2025-08-04 20:39:50 +0900

    test: git rule 테스트

--- src/main/java/org/kc5/learningmate/api/v1/controller/ArticleController.java
+++ src/main/java/org/kc5/learningmate/api/v1/controller/ArticleController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//git rule test
 @RequestMapping("/api/v1/articles")
 @RequiredArgsConstructor
 @RestController
