9d1a99af25ec777e436933c49324e659e13c7dd0
hyeji <hyeji6742@naver.com>
2025-08-07 19:34:17 +0900

    refactor: @EnableJpaAuditing 별도의 config 파일로 관리

---
+++ src/main/java/org/kc5/learningmate/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package org.kc5.learningmate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
