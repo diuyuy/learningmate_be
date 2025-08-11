f169af437fdbc1c0d16b281c7356e9fb47ef32e5
hyeji <hyeji6742@naver.com>
2025-08-11 20:15:55 +0900

    feat: 기사에 대한 퀴즈 조회 기능 구현 #21

--- src/main/java/org/kc5/learningmate/domain/quiz/repository/QuizRepository.java
+++ src/main/java/org/kc5/learningmate/domain/quiz/repository/QuizRepository.java
@@ -4,6 +4,9 @@ import org.kc5.learningmate.domain.quiz.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
+    Quiz findByArticleId(Long articleId);
 }
