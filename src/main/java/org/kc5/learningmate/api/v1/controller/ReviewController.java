1d6d675ae5074cdf41999254331bc9186980f31e
hyeji <hyeji6742@naver.com>
2025-08-11 16:31:10 +0900

    refactor: ResponseEntity로 반환값 통일 #29

--- src/main/java/org/kc5/learningmate/api/v1/controller/ReviewController.java
+++ src/main/java/org/kc5/learningmate/api/v1/controller/ReviewController.java
@@ -11,6 +11,7 @@ import org.kc5.learningmate.api.v1.dto.response.ReviewResponse;
 import org.kc5.learningmate.common.ResultResponse;
 import org.kc5.learningmate.domain.review.service.ReviewService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1")
@@ -23,35 +24,40 @@ public class ReviewController {
 
     @Operation(summary = "기사 상세 리뷰 작성", description = "한 개의 기사에 대해 리뷰를 작성합니다.")
     @PostMapping("/articles/{articleId}/reviews")
-    public ResultResponse<Void> createReview(@PathVariable("articleId") Long articleId,
+    public ResponseEntity<ResultResponse<Void>> createReview(@PathVariable("articleId") Long articleId,
                                              @Valid @RequestBody ReviewCreateRequest request) {
         reviewService.createReview(articleId, request);
-        return new ResultResponse<>(HttpStatus.CREATED);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResultResponse<>(HttpStatus.CREATED));
     }
 
     @Operation(summary = "기사 상세 내 리뷰 조회", description = "특정 기사에 대해 작성한 나의 리뷰를 조회 합니다.")
     @GetMapping("/articles/{articleId}/reviews")
-    public ResultResponse<ReviewResponse> getReview(@PathVariable("articleId") Long articleId,
+    public ResponseEntity<ResultResponse<ReviewResponse>> getReview(@PathVariable("articleId") Long articleId,
                                                     @RequestBody ReviewGetRequest request) {
         ReviewResponse response = reviewService.getReview(articleId, request.getMemberId());
-        return new ResultResponse<>(response);
+        return ResponseEntity
+                .ok(new ResultResponse<>(response));
     }
 
     @Operation(summary = "기사 상세 리뷰 수정", description = "한 개의 기사에 대해 리뷰를 수정 합니다.")
     @PatchMapping("/articles/{articleId}/reviews/{reviewId}")
-    public ResultResponse<Void> updateReview(@PathVariable("articleId") Long articleId,
+    public  ResponseEntity<ResultResponse<Void>> updateReview(@PathVariable("articleId") Long articleId,
                                              @PathVariable("reviewId") Long reviewId,
                                              @Valid @RequestBody ReviewUpdateRequest request) {
         reviewService.updateReview(articleId, reviewId, request);
-        return new ResultResponse<>();
+        return ResponseEntity
+                .ok(new ResultResponse<>());
     }
 
     @Operation(summary = "기사 상세 리뷰 삭제", description = "한 개의 기사에 대해 리뷰를 삭제 합니다.")
     @DeleteMapping("/articles/{articleId}/reviews/{reviewId}")
-    public ResultResponse<Void> createReview(@PathVariable("articleId") Long articleId,
+    public  ResponseEntity<ResultResponse<Void>> createReview(@PathVariable("articleId") Long articleId,
                                              @PathVariable("reviewId") Long reviewId) {
         reviewService.deleteReview(articleId, reviewId);
-        return new ResultResponse<>();
+        return ResponseEntity
+                .ok(new ResultResponse<>());
     }
 
 }
