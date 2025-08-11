7ad255ebe2392428cc3b149a12197478b0b3427f
diuyuy <yshyun0105@gmail.com>
2025-08-11 15:43:57 +0900

    feat: 특정 기간의 키워드 목록 조회 #11

---
+++ src/main/java/org/kc5/learningmate/api/v1/dto/response/TodaysKeywordDto.java
@@ -0,0 +1,27 @@
+package org.kc5.learningmate.api.v1.dto.response;
+
+import lombok.Builder;
+import lombok.Value;
+import org.kc5.learningmate.domain.keyword.entity.TodaysKeyword;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * DTO for {@link org.kc5.learningmate.domain.keyword.entity.TodaysKeyword}
+ */
+@Value
+@Builder
+public class TodaysKeywordDto implements Serializable {
+    Long id;
+    KeywordDto keyword;
+    LocalDate date;
+
+    public static TodaysKeywordDto fromEntityWithKeyword(TodaysKeyword todaysKeyword) {
+        return TodaysKeywordDto.builder()
+                               .id(todaysKeyword.getId())
+                               .keyword(KeywordDto.fromEntity(todaysKeyword.getKeyword()))
+                               .date(todaysKeyword.getDate())
+                               .build();
+    }
+}
