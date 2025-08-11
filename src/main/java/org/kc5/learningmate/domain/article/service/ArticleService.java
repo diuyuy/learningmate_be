4829851da9ed8fdf9eb2ac2fca57a585bb2c8326
hyeji <hyeji6742@naver.com>
2025-08-11 20:15:55 +0900

    feat: 기사에 대한 퀴즈 조회 기능 구현 #21

--- src/main/java/org/kc5/learningmate/domain/article/service/ArticleService.java
+++ src/main/java/org/kc5/learningmate/domain/article/service/ArticleService.java
@@ -1,9 +1,33 @@
 package org.kc5.learningmate.domain.article.service;
 
 import lombok.RequiredArgsConstructor;
+import org.kc5.learningmate.api.v1.dto.response.QuizResponse;
+import org.kc5.learningmate.common.exception.CommonException;
+import org.kc5.learningmate.common.exception.ErrorCode;
+import org.kc5.learningmate.domain.article.repository.ArticleRepository;
+import org.kc5.learningmate.domain.quiz.entity.Quiz;
+import org.kc5.learningmate.domain.quiz.repository.QuizRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+    private final QuizRepository quizRepository;
+
+    @Transactional(readOnly = true)
+    public QuizResponse getQuiz(Long articleId) {
+        articleRepository.findById(articleId)
+                .orElseThrow(() -> new CommonException(ErrorCode.ARTICLE_NOT_FOUND));
+
+        Quiz quiz = quizRepository.findByArticleId(articleId);
+
+        return QuizResponse.from(quiz);
+
+    }
+
 }
