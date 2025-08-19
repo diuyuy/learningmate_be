958f77fbe5d2535e0f69213373f09257a223437a
hyeji <hyeji6742@naver.com>
2025-08-19 17:08:25 +0900

    refactor: 리뷰 반í 단일 조회 =   ã조회 반환 값 수정 #29

--- src/main/java/org/kc5/learningmate/api/v1/dto/response/MyReviewResponse.java
+++ src/main/java/org/kc5/learningmate/api/v1/dto/response/MyReviewResponse.java
@@ -12,11 +12,14 @@ import org.kc5.learningmate.domain.review.entity.Review;
 @Schema(description = "리뷰 작성 응답 모델")
 public class MyReviewResponse {
 
-    @Schema(description = "회원 정보")
-    private String nickname;
+    @Schema(description = "리뷰 번호")
+    private Long id;
 
     @Schema(description = "기사 정보")
-    private String title;
+    private ArticleResponse article;
+
+    @Schema(description = "회원 정보")
+    private Long memberId;
 
     @Schema(description = "기사에 대한 내 생각")
     private String content1;
@@ -30,14 +33,13 @@ public class MyReviewResponse {
     // Entity -> DTO 변환 메서드
     public static MyReviewResponse from(Review review) {
         return MyReviewResponse.builder()
-                               .nickname(review.getMember()
-                                               .getNickname())
-                               .title(review.getArticle()
-                                            .getTitle())
-                               .content1(review.getContent1())
-                               .content2(review.getContent2())
-                               .content3(review.getContent3())
-                               .build();
+            .id(review.getId())
+            .article(ArticleResponse.from(review.getArticle()))
+           .memberId(review.getMember().getId())
+           .content1(review.getContent1())
+           .content2(review.getContent2())
+           .content3(review.getContent3())
+           .build();
     }
 
 }
